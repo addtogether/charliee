a7071ca123ba4ee27781c1d24cc8998c1064b292
Vishal Pandey <vishalpandey917@gmail.com>
2022-06-01 13:19:53 +0530

    api

--- api/login.php
+++ api/login.php
@@ -39,20 +39,18 @@
         }
         else{
             $sql = mysqli_query($conn,"SELECT * FROM employeeLogin WHERE username = '{$decoded['username']}'");
-            if($sql){
+            if(mysqli_num_rows($sql) != 0 && $check){
                 $row = mysqli_fetch_assoc($sql);
                 $check = password_verify($decoded['password'], $row['password']);
-                if(mysqli_num_rows($sql) != 0 && $check){
-                    if($row['status']=="ON"){
-                        $sql = mysqli_query($conn, "INSERT INTO employeeLoginLog (username, loginDateTime, loginGeoLocation) VALUES ('{$decoded['username']}', CURRENT_TIMESTAMP, '{$decoded['geolocation']}')");
-                        // echo("Error description: " . mysqli_error($conn));
-                    }
-                    unset($row['password']);
-                    echo json_encode($row);
+                if($row['status']=="ON"){
+                    $sql = mysqli_query($conn, "INSERT INTO employeeLoginLog (username, loginDateTime, loginGeoLocation) VALUES ('{$decoded['username']}', CURRENT_TIMESTAMP, '{$decoded['geolocation']}')");
+                    // echo("Error description: " . mysqli_error($conn));
                 }
-                else{
-                    echo json_encode(["validation"=>false]);
-                }
+                unset($row['password']);
+                echo json_encode($row);
+            }
+            else{
+                echo json_encode(["validation"=>false]);
             }
         }
     }
