94c47f874d4f92850f156e971d8fc5f6f356ccc1
Vishal Pandey <vishalpandey917@gmail.com>
2022-06-09 15:51:50 +0530

    show route

--- js/navbar.js
+++ js/navbar.js
@@ -5,7 +5,7 @@ dashboard = document.getElementById("menu-dashboard");
 employee = document.getElementById("menu-employee");
 retailer = document.getElementById("menu-retailer");
 route = document.getElementById("menu-route");
-route = document.getElementById("menu-routeShow");
+routeShow = document.getElementById("menu-routeShow");
 
 
 dashboard.classList.remove("active");
@@ -27,7 +27,7 @@ else if(filename=="retailer.php" || filename=="addRetailer.php"){
 else if(filename=="employeeRoute.php"){
     route.classList.add("active");
 }
-else if(filename=="employeeRoute.php"){
+else if(filename=="routeShow.php"){
     routeShow.classList.add("active");
 }
 
