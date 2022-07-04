9df7ec20ffd2f5546cc18ff1ee9e091a5f5d1768
Aamir195 <aamirer195@gmail.com>
2022-07-04 10:40:10 +0530

    modal done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,74 +19,80 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = "none";pop
   }
 }
 
 // Export Excel
-function exportTableToExcel(tableID, filename = ''){
+function exportTableToExcel(tableID, filename = '') {
   var downloadLink;
   var dataType = 'application/vnd.ms-excel';
   var tableSelect = document.getElementById(tableID);
   var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-  
+
   // Specify file name
-  filename = filename?filename+'.xls':'excel_data.xls';
-  
+  filename = filename ? filename + '.xls' : 'excel_data.xls';
+
   // Create download link element
   downloadLink = document.createElement("a");
-  
+
   document.body.appendChild(downloadLink);
-  
-  if(navigator.msSaveOrOpenBlob){
-      var blob = new Blob(['\ufeff', tableHTML], {
-          type: dataType
-      });
-      navigator.msSaveOrOpenBlob( blob, filename);
-  }else{
-      // Create a link to the file
-      downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-  
-      // Setting the file name
-      downloadLink.download = filename;
-      
-      //triggering the function
-      downloadLink.click();
+
+  if (navigator.msSaveOrOpenBlob) {
+    var blob = new Blob(['\ufeff', tableHTML], {
+      type: dataType
+    });
+    navigator.msSaveOrOpenBlob(blob, filename);
+  } else {
+    // Create a link to the file
+    downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+    // Setting the file name
+    downloadLink.download = filename;
+
+    //triggering the function
+    downloadLink.click();
   }
 }
 // year picker 
-$("#datepicker").datepicker( {
+$("#datepicker").datepicker({
   format: "mm-yyyy",
-  startView: "months", 
+  startView: "months",
   minViewMode: "months"
 });
 
 /// draggable row in table 
 var row;
 
-function start(){  
-  row = event.target; 
+function start() {
+  row = event.target;
 }
-function dragover(){
+function dragover() {
   var e = event;
-  e.preventDefault(); 
-  
-  let children= Array.from(e.target.parentNode.parentNode.children);
-  
-  if(children.indexOf(e.target.parentNode)>children.indexOf(row))
+  e.preventDefault();
+
+  let children = Array.from(e.target.parentNode.parentNode.children);
+
+  if (children.indexOf(e.target.parentNode) > children.indexOf(row))
     e.target.parentNode.after(row);
   else
     e.target.parentNode.before(row);
 }
+
+// for the modal :- start
+// Get the modal
+
+
+///  for the modal :- ends 
--- retailersOrder.php
+++ retailersOrder.php
@@ -23,26 +23,68 @@ require_once("./includes/connection.php");
 include_once("navbar.php");
 ?>
 
+<style>
+    .popup1 {
+        background: rgba(0, 0, 0, 0.65);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: fixed;
+        opacity: 0;
+        pointer-events: none;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        z-index: 100000;
+        transition: all 0.3s ease-in-out;
+    }
+
+    .popup1.show {
+        opacity: 1;
+        pointer-events: auto;
+    }
+
+    .popup1 .box {
+        align-self: center;
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: 0 10px 20px rgba(104, 80, 80, 0.2);
+        padding: 15px 20px;
+        width: 35%;
+        margin: 10px auto;
+        cursor: pointer;
+    }
+
+    .popup1 .box .part-1 h3 {
+        color: #1cc88a;
+        font-weight: bold;
+    }
+
+    .close {
+        float: right;
+        size: 14px;
+    }
+</style>
+
 <!-- Main Content -->
 <div class="main-content">
     <section class="section">
         <div class="section-body">
             <div class="row">
-                <div class="col-12">
+                <div class="col-md-12 col-lg-12 col-xl-12">
                     <div class="card">
                         <div class="card-header">
-                            <h5>Vishal Kumar</h5>
+                            <h4>Vishal Kumar</h4>
                             <div class="verticalLine">
-                                <h5>Dadar East</h5>
+                                <h4>Dadar East</h4>
                             </div>
                             <div class="verticalLine">
-                                <h5>11/5/2022</h5>
+                                <h4>11/5/2022</h4>
                             </div>
                             <div class="verticalLine">
-                            <h5>XYZ Company</h5>
+                                <h4>XYZ Company</h4>
                             </div>
-                            
-
                         </div>
                         <div class="card-body">
                             <div class="table-responsive user-table">
@@ -53,6 +95,7 @@ include_once("navbar.php");
                                         <th>Order Status</th>
                                         <th>Total Amount ₹</th>
                                         <th>Total Quantity</th>
+                                        <th>Edit</th>
                                     </thead>
                                     <tbody id="routeList">
                                         <!-- <td colspan="3">No Elements</td> -->
@@ -62,6 +105,9 @@ include_once("navbar.php");
                                             <td><span class="status-p bg-correct">Delivered</span></td>
                                             <td>1,1300.00</td>
                                             <td>34</td>
+                                            <td>
+                                                <a href="#" class="btn btn-danger btn-delete btn-sm" id="open">Edit</a>
+                                            </td>
 
                                         </tr>
                                         <tr>
@@ -115,6 +161,45 @@ include_once("navbar.php");
                         </div>
                     </div>
                 </div>
+
+                <!-- modal start -->
+                <div id="popup1" class="popup1">
+                    <div class="box">
+                        <span class="close" id="close">&times;</span>
+                        <div class="part-1">
+                            <h3>Order Status</h3>
+                        </div>
+
+                        <form action="" method="">
+                            <div class="form-row">
+                                <div class="content">
+                                    <!-- Any Content -->
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group">
+                                    <div class="col-md-12 mb-3">
+
+                                        <label class="col-form-label">Status</label>
+                                        <select class="form-control" name="status" id="status" required>
+                                            <option selected disabled value="">Select Status</option>
+                                            <option>Delivered</option>
+                                            <option>Refunded</option>
+                                            <option>Pending</option>
+                                            
+                                        </select>
+                                        <div class="invalid-feedback">
+                                            Please select a Valid Status.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btns btn-approve btn-warning" id="close">Update Status</button>
+                        </form>
+                    </div>
+                </div>
+                <!-- modal ends -->
+
             </div>
         </div>
     </section>
@@ -123,6 +208,46 @@ include_once("navbar.php");
     include_once("settings.php");
     ?>
 </div>
+<!-- script for modal  -->
+<script>
+    const btnOpen = document.querySelector('#open');
+    const btnClose = document.querySelector('#close');
+
+    const popupContainer = document.querySelector('.popup1');
+    const boxContent = document.querySelector('.content');
+
+    var modal = document.getElementById('popup1')
+
+    btnOpen.addEventListener('click', () => {
+
+        const component = btnOpen.parentElement.parentElement;
+        console.log(component);
+
+        for (let i = 0; i < component.cells.length - 1; i++) {
+            boxContent.innerHTML += component.cells[i].innerHTML + ' ';
+        }
+
+        // for getting only one element 
+        // let element =1;
+        // boxContent.innerHTML = component.cells[i].innerHTML;
+
+        console.log(popupContainer);
+        popupContainer.classList.add('show');
+
+    });
+
+    btnClose.addEventListener('click', () => {
+        popupContainer.classList.remove('show')
+        boxContent.innerHTML = '';
+    });
+
+    // window.onclick = function(event) {
+    //     if (event.target == popup1) {
+    //         popup1.style.display = "none";
+    //     }
+    // }
+</script>
+
 <!-- General JS Scripts -->
 <script src="assets/js/app.min.js"></script>
 <!-- JS Libraies -->
