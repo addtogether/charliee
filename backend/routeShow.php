42b420525cbcd9932252174699df26be51ed6f13
Vishal Pandey <vishalpandey917@gmail.com>
2022-07-14 18:15:55 +0530

    moved route name to top

--- backend/routeShow.php
+++ backend/routeShow.php
@@ -8,8 +8,8 @@
         // echo $designationDropdown;
         $sql = mysqli_query($conn, "SELECT id,routeName FROM routeMaster WHERE assignToEmployee = '{$employee}' AND weekDay = '{$day}'");
         if(mysqli_num_rows($sql) != 0){
-            echo '<tr><td colspan="3"><h4>'.$row['routeName'].'</h4></td></tr>';
             $row = mysqli_fetch_assoc($sql);
+            echo '<tr><td colspan="3"><h4>'.$row['routeName'].'</h4></td></tr>';
             $sql1 = mysqli_query($conn, "SELECT retailerID, priority, status FROM routeRetailerMapping WHERE routeID = '{$row['id']}' ORDER BY priority");
             while($row1 = mysqli_fetch_assoc($sql1)){
                 $sql2 = mysqli_query($conn, "SELECT retailerName FROM retailerMaster WHERE id = '{$row1['retailerID']}'");
