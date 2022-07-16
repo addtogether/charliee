e1251e1ffc2557298f15e2880c5cdb7205ec8cf8
Aamir195 <aamirer195@gmail.com>
2022-07-16 21:26:00 +0530

    valiant server uncomment

--- includes/connection.php
+++ includes/connection.php
@@ -1,16 +1,16 @@
 <?php
     date_default_timezone_set("Asia/Kolkata");
     //clever cloud
-    $servername = "bfuzsgiszke6l2e7z5o9-mysql.services.clever-cloud.com";
-    $username = "u7ttomsnlwm1gcxl";
-    $password = "9Vd30iY4A0KjinAF8WSu";
-    $dbname = "bfuzsgiszke6l2e7z5o9";
+    // $servername = "bfuzsgiszke6l2e7z5o9-mysql.services.clever-cloud.com";
+    // $username = "u7ttomsnlwm1gcxl";
+    // $password = "9Vd30iY4A0KjinAF8WSu";
+    // $dbname = "bfuzsgiszke6l2e7z5o9";
 
     //valiant
-    // $servername = "localhost";
-    // $username = "valiantn_user";
-    // $password = "Nikul@111$";
-    // $dbname = "valiantn_db";
+    $servername = "localhost";
+    $username = "valiantn_user";
+    $password = "Nikul@111$";
+    $dbname = "valiantn_db";
     
     $conn = mysqli_connect($servername,$username,$password,$dbname);
     if(!$conn){
